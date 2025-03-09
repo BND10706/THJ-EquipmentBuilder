745d16c6a8c234143bcbfbb7c250a72bc8bbf554
Brandon DeVore <46546477+BND10706@users.noreply.github.com>
2025-03-09 17:16:12 -0400

    fix: update SVG paths to include public directory

--- lib/equipment-icons.ts
+++ lib/equipment-icons.ts
@@ -3,39 +3,39 @@ import { getAssetPath } from './utils';
 // Map of equipment slots to their corresponding icon files
 export const EQUIPMENT_ICONS = {
   // Head area
-  Head: getAssetPath('/icons/equipment/head.svg'),
-  Face: getAssetPath('/icons/equipment/face.svg'),
-  Ear1: getAssetPath('/icons/equipment/ear.svg'),
-  Ear2: getAssetPath('/icons/equipment/ear.svg'),
+  Head: getAssetPath('/public/icons/equipment/head.svg'),
+  Face: getAssetPath('/public/icons/equipment/face.svg'),
+  Ear1: getAssetPath('/public/icons/equipment/ear.svg'),
+  Ear2: getAssetPath('/public/icons/equipment/ear.svg'),
   
   // Torso area
-  Chest: getAssetPath('/icons/equipment/chest.svg'),
-  Back: getAssetPath('/icons/equipment/back.svg'),
-  Shoulders: getAssetPath('/icons/equipment/shoulders.svg'),
-  Arms: getAssetPath('/icons/equipment/arms.svg'),
-  Hands: getAssetPath('/icons/equipment/hands.svg'),
+  Chest: getAssetPath('/public/icons/equipment/chest.svg'),
+  Back: getAssetPath('/public/icons/equipment/back.svg'),
+  Shoulders: getAssetPath('/public/icons/equipment/shoulders.svg'),
+  Arms: getAssetPath('/public/icons/equipment/arms.svg'),
+  Hands: getAssetPath('/public/icons/equipment/hands.svg'),
   
   // Accessories
-  Neck: getAssetPath('/icons/equipment/neck.svg'),
-  Wrist1: getAssetPath('/icons/equipment/wrist.svg'),
-  Wrist2: getAssetPath('/icons/equipment/wrist.svg'),
-  Belt: getAssetPath('/icons/equipment/belt.svg'),
-  Ring1: getAssetPath('/icons/equipment/ring.svg'),
-  Ring2: getAssetPath('/icons/equipment/ring.svg'),
+  Neck: getAssetPath('/public/icons/equipment/neck.svg'),
+  Wrist1: getAssetPath('/public/icons/equipment/wrist.svg'),
+  Wrist2: getAssetPath('/public/icons/equipment/wrist.svg'),
+  Belt: getAssetPath('/public/icons/equipment/belt.svg'),
+  Ring1: getAssetPath('/public/icons/equipment/ring.svg'),
+  Ring2: getAssetPath('/public/icons/equipment/ring.svg'),
   
   // Lower body
-  Legs: getAssetPath('/icons/equipment/legs.svg'),
-  Feet: getAssetPath('/icons/equipment/feet.svg'),
+  Legs: getAssetPath('/public/icons/equipment/legs.svg'),
+  Feet: getAssetPath('/public/icons/equipment/feet.svg'),
   
   // Special items
-  Charm: getAssetPath('/icons/equipment/charm.svg'),
-  Power: getAssetPath('/icons/equipment/power.svg'),
+  Charm: getAssetPath('/public/icons/equipment/charm.svg'),
+  Power: getAssetPath('/public/icons/equipment/power.svg'),
   
   // Weapons and ammo
-  Primary: getAssetPath('/icons/equipment/weapon.svg'),
-  Secondary: getAssetPath('/icons/equipment/shield.svg'),
-  Range: getAssetPath('/icons/equipment/bow.svg'),
-  Ammo: getAssetPath('/icons/equipment/ammo.svg'),
+  Primary: getAssetPath('/public/icons/equipment/weapon.svg'),
+  Secondary: getAssetPath('/public/icons/equipment/shield.svg'),
+  Range: getAssetPath('/public/icons/equipment/bow.svg'),
+  Ammo: getAssetPath('/public/icons/equipment/ammo.svg'),
 } as const;
 
 // Type for equipment slots
